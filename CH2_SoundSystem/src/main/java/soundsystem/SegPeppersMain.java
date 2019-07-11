ca98d13c8ff1a141c047f8306a0102991b669412
Yuwei <winnie_lin0712@163.com>
2019-07-11 18:07:51 +0800

    autowired bean

---
+++ CH2_SoundSystem/src/main/java/soundsystem/SegPeppersMain.java
@@ -0,0 +1,8 @@
+package soundsystem;
+
+public class SegPeppersMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ CH2_SoundSystem/src/main/java/soundsystem/SgtPeppers.java
@@ -0,0 +1,10 @@
+package soundsystem;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SgtPeppers implements CompactDisc {
+    public void play() {
+
+    }
+}
