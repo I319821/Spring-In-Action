045921ae85c43f84dd8bd1561c19f453c949ea17
Yuwei <winnie_lin0712@163.com>
2019-07-12 11:29:12 +0800

    test

--- CH2_SoundSystem/src/test/java/soundsystem/CDPlayerTest.java
+++ CH2_SoundSystem/src/test/java/soundsystem/CDPlayerTest.java
@@ -6,11 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
 import static junit.framework.TestCase.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = CDPlayerConfig.class)
 public class CDPlayerTest {
+
+    @Autowired
+    private MediaPlayer player;
 
     @Autowired
     private CompactDisc cd;
